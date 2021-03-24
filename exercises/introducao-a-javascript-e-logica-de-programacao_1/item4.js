92d22c914fce82cadf6df19e3d51c3fd4bae3762
Leandro Lopes <leandro_carlos@uol.com.br>
2021-03-24 19:13:57 -0300

    programa 4 de js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/item4.js
@@ -0,0 +1,15 @@
+let num1 = 20
+let num2 = 10
+let subtração = num1 - num2
+
+if (subtração > 0) {
+    console.log('positive')
+}
+
+else if (subtração < 0){
+    console.log('negative')
+}
+
+else {
+    console.log('zero')
+}
