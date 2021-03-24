32270e83c580673ff1bb1cfe341fc723e8d9d997
Leandro Lopes <leandro_carlos@uol.com.br>
2021-03-24 19:00:57 -0300

    programa 3 de js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/item3.js
@@ -0,0 +1,15 @@
+let num1 = 180
+let num2 = 350
+let num3 = 880
+
+if (num1 > num2) {
+    console.log(num1)
+}
+
+else if (num2 > num3){
+    console.log(num2)
+}
+
+else {
+    console.log(num3)
+}
